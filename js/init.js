c2255b879b7a7455f8f4aa6411149d65e43ba3ed
balaji-venkatesh <balaji.venkatesh@hotmail.com>
2019-04-13 20:43:04 -0400

    will remove

--- js/init.js
+++ js/init.js
@@ -4,5 +4,6 @@
     $('.button-collapse').sideNav();
     $('.carousel').carousel();
     $('.dropdown-trigger').dropdown();
+    $('.slider').slider();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
