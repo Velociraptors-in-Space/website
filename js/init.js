c47160d1f364f700cb63838a94417e4219cfb68e
Jim42069 <timothychan168@gmail.com>
2019-04-16 18:16:52 -0400

    fix

--- js/init.js
+++ js/init.js
@@ -1,8 +1,11 @@
-(function($){
-  $(function(){
-
+(function ($) {
+  $(function () {
+    $('.materialboxed').materialbox();
     $('.button-collapse').sideNav();
     $('.carousel').carousel();
     $('.dropdown-trigger').dropdown();
+    $('.scrollspy').scrollSpy();
+    $(".dropdown-button").dropdown();
+    $(".collapsible").collapsible();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
